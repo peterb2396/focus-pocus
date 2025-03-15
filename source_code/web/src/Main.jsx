fee8e9036252717ab76bd5861bd9474265098fd6
Peter <63479070+peterb2396@users.noreply.github.com>
2025-03-15 16:13:16 -0400

    width

--- source_code/web/src/Main.jsx
+++ source_code/web/src/Main.jsx
@@ -400,7 +400,7 @@ const renderListView = () => {
         boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
         padding: '1.5rem',
         marginBottom: '1.5rem',
-        height: '70vh',
+        height: '60vh',
         display: 'flex',
         flexDirection: 'column'
       }}>
